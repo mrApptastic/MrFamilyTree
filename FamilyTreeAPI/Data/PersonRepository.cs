e636fb73687206d070a342cc30c37cf1f4435b36
mrApptastic <30831839+mrApptastic@users.noreply.github.com>
2025-01-02 21:25:42 +0100

    API: Multiple generations.

--- FamilyTreeAPI/Data/PersonRepository.cs
+++ FamilyTreeAPI/Data/PersonRepository.cs
@@ -4,16 +4,17 @@ using FamilyTreeAPI.Models;
 
 namespace FamilyTreeAPI.Data
 {
-	public interface IPersonRepository {
+    public interface IPersonRepository
+    {
         Task<PersonView> GetPersonAsync(Guid externalId, bool onlyEnabledForWeb = true);
     }
 
-    public class PersonRepository: IPersonRepository
+    public class PersonRepository : IPersonRepository
     {
         private readonly ILogger<PersonRepository> _logger;
         private readonly IMapper _mapper;
         private readonly ApplicationDbContext _context;
-        
+
         public PersonRepository(ILogger<PersonRepository> logger, IMapper mapper, ApplicationDbContext context)
         {
             _logger = logger;
@@ -21,23 +22,53 @@ namespace FamilyTreeAPI.Data
             _context = context;
         }
 
-        public async Task<PersonView> GetPersonAsync(Guid id, bool onlyPublic = true) 
+        public async Task<PersonView> GetPersonWithGenerationsAsync(Guid id, bool onlyPublic = true)
+        {
+            var personWithGenerations =
+            await _context.FamilyTreePersons
+                    .Include(p => p.Mother)
+                        .ThenInclude(m => m.Mother)
+                            .ThenInclude(mm => mm.Mother)
+                                .ThenInclude(mmm => mmm.Mother)
+                                    .ThenInclude(mmmm => mmmm.Mother)
+                    .Include(p => p.Mother)
+                        .ThenInclude(m => m.Father)
+                            .ThenInclude(mf => mf.Mother)
+                                .ThenInclude(mfm => mfm.Mother)
+                                    .ThenInclude(mfmm => mfmm.Mother)
+                    .Include(p => p.Father)
+                        .ThenInclude(f => f.Mother)
+                            .ThenInclude(fm => fm.Mother)
+                                .ThenInclude(fmm => fmm.Mother)
+                                    .ThenInclude(fmmm => fmmm.Mother)
+                    .Include(p => p.Father)
+                        .ThenInclude(f => f.Father)
+                            .ThenInclude(ff => ff.Mother)
+                                .ThenInclude(ffm => ffm.Mother)
+                                    .ThenInclude(ffmm => ffmm.Mother)
+                 .FirstOrDefaultAsync(p => p.Id == id);
+
+            return _mapper.Map<Person, PersonView>(personWithGenerations);
+        }
+
+        public async Task<PersonView> GetPersonAsync(Guid id, bool onlyPublic = true)
         {
             var person = await _context.FamilyTreePersons
-                                       .Where(x => x.Id == id && 
+                                       .Where(x => x.Id == id &&
                                                    (onlyPublic ? x.Public : true)
                                         )
                                         .Include(x => x.Mother).ThenInclude(y => y.Father).ThenInclude(y => y.Mother)
                                         .Include(x => x.Father).ThenInclude(y => y.Father).ThenInclude(y => y.Mother)
                                         .FirstOrDefaultAsync();
-            
-            if (person == null) {
+
+            if (person == null)
+            {
                 var expt = new ArgumentException("The requested person does not exist");
                 _logger.LogError(expt.Message);
                 throw expt;
             }
 
             return _mapper.Map<Person, PersonView>(person);
-        }        
+        }
     }
 }
